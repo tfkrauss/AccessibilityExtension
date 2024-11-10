a8c615b8601cbe21eddb40bedd1654a7e43c97f6
Tyler Krauss <marjkrauss@Marjs-MacBook-Air.local>
2024-11-09 20:15:15 -0800

    Implemented text highlighting -- switched click event for mouseup

--- content.js
+++ content.js
@@ -6,8 +6,10 @@ function addHoverEffect(){
 
     //Add hover effect on all divs
     divs.forEach(div => {    
-
+        
+        /*
         // CHECK FOR INVALID NONTEXTUAL DIVS
+        */
         const hasOnlyInlineChildren = Array.from(div.children).every(child => {
             const display = window.getComputedStyle(child).display;
             return display === 'inline' || display === 'inline-block';
@@ -17,16 +19,16 @@ function addHoverEffect(){
         if (
             !hasOnlyInlineChildren ||
             div.tagName === 'A' ||     // Is a link
-            div.tagName === 'BUTTON' ||// Is a button
+            div.tagName === 'BUTTON' || // Is a button
             div.hasAttribute('onclick') // Has onclick event
         ) {
             return; // Skip this element
         }
         
 
-
+        /*
         //OUTLINE ON HOVER EVENTS
-
+        */
         function getEffectiveBackgroundColor(el) {
             let backgroundColor = window.getComputedStyle(el).backgroundColor;
             let currentElement = el;
@@ -58,25 +60,39 @@ function addHoverEffect(){
         // Determine the border color based on the background color
         const bgColor = getEffectiveBackgroundColor(div);
         const borderColor = isDarkBackground(bgColor) ? 'white' : 'black';
-
+        const origCursor = div.style.cursor;
         const origOutline = div.style.outline;
+
         div.addEventListener("mouseover", () => {
             div.style.outline = `2px solid ${borderColor}`;
-            div.style.cursor = "pointer";
+            //div.style.cursor = "pointer";
         })
         
         div.addEventListener("mouseout", () => {
             div.style.outline = origOutline;
+            div.style.cursor = origCursor;
         })
-        
 
 
-        //CLICK EVENTS
-        div.addEventListener("click", () => {
-            const text = div.textContent;
-            console.log(text);
-        })
+        /*
+        //TEXT SELECTION ON MOUSEUP EVENT & CLICK EVENT
+        */
+        // Check for selected text on mouseup
+        div.addEventListener("mouseup", () => {
+            const selectedText = window.getSelection().toString().trim();
+            if (selectedText) {
+                // Call function to summarize selected text
+                summarizeText(selectedText);
+            } else {
+                const text = div.textContent;
+                console.log(text);
+            }
+        });
     })
+}
+
+function summarizeText(text) {
+    console.log(text);
 }
 
 
