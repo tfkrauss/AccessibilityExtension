75dadaab7607040adba2c91ae4d5a128e25b6ee1
baileyyo <111161049+byoung22@users.noreply.github.com>
2024-11-09 16:26:01 -0800

    Connected to background and content

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('changeColor').addEventListener('click', function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.executeScript(tabs[0].id, {
-      code: 'document.body.style.backgroundColor = "lightblue";'
-    });
-  });
-});
